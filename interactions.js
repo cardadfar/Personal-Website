6d2362b085e87fbb157b22ae85b02e0cc62a4d62
LAPTOP-6QBLE82J\Oscar Dadfar <cardadfar@gmail.com>
2018-02-27 21:19:41 -0500

    twenty-fourth commit

--- interactions.js
+++ interactions.js
@@ -82,59 +82,52 @@ $(".cover-image-right").hover(function() {
 
 $("#idance").click(function() {
     $("iframe").attr("src","https://player.vimeo.com/video/257607289");
-    $(".video-player").addClass("video-player-on");
-    $(".video-bg").addClass("video-player-on");
-    $(".dark-frame").addClass("dark-frame-on");
+    loadFrame();
 });
 
 $("#break-free").click(function() {
     $("iframe").attr("src","https://player.vimeo.com/video/253337182");
-    $(".video-player").addClass("video-player-on");
-    $(".video-bg").addClass("video-player-on");
-    $(".dark-frame").addClass("dark-frame-on");
+    loadFrame();
 });
 
 $("#peace-sign").click(function() {
     $("iframe").attr("src","https://player.vimeo.com/video/250210552");
-    $(".video-player").addClass("video-player-on");
-    $(".video-bg").addClass("video-player-on");
-    $(".dark-frame").addClass("dark-frame-on");
+    loadFrame();
 });
 
 $("#null-space").click(function() {
     $("iframe").attr("src","https://player.vimeo.com/video/249094937");
-    $(".video-player").addClass("video-player-on");
-    $(".video-bg").addClass("video-player-on");
-    $(".dark-frame").addClass("dark-frame-on");
+    loadFrame();
 });
 
 $("#infinite").click(function() {
     $("iframe").attr("src","https://player.vimeo.com/video/249187332");
-    $(".video-player").addClass("video-player-on");
-    $(".video-bg").addClass("video-player-on");
-    $(".dark-frame").addClass("dark-frame-on");
+    loadFrame();
 });
 
 $("#plastic-promises").click(function() {
     $("iframe").attr("src","https://player.vimeo.com/video/249258739");
-    $(".video-player").addClass("video-player-on");
-    $(".video-bg").addClass("video-player-on");
-    $(".dark-frame").addClass("dark-frame-on");
+    loadFrame();
 });
 
 $("#stitches").click(function() {
     $("iframe").attr("src","https://player.vimeo.com/video/249324773");
-    $(".video-player").addClass("video-player-on");
-    $(".video-bg").addClass("video-player-on");
-    $(".dark-frame").addClass("dark-frame-on");
+    loadFrame();
 });
 
 $("#rondo").click(function() {
     $("iframe").attr("src","https://player.vimeo.com/video/250281441");
+    loadFrame();
+});
+
+function loadFrame() {
     $(".video-player").addClass("video-player-on");
     $(".video-bg").addClass("video-player-on");
     $(".dark-frame").addClass("dark-frame-on");
-});
+    setTimeout(function() {
+        $(".video-bg").addClass("video-bg-error");
+    }, 2000);
+}
 
 $("#web-dev").click(function() {
     window.location.href = "https://cardadfar.github.io/Web-Dev-Weekend-Website/index.html";
@@ -156,6 +149,7 @@ $(".dark-frame").click(function() {
     $(".video-player").removeClass("video-player-on");
     $(".video-bg").removeClass("video-player-on");
     $(".dark-frame").removeClass("dark-frame-on");
+    $(".video-bg").removeClass("video-bg-error");
     setTimeout(function() {
         $("iframe").attr("src","");
     }, 500);
